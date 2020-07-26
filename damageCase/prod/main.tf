5cc9cf332322d024c052561827cee6af0ef267fc
Lars Willrich <larswillrich@gmail.com>
2020-07-26 21:57:51 +0000

    added s3 bucket to int and prod

---
+++ damageCase/prod/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-lars-willrich"
+    key            = "larswillrich/myInsuranceExample/terraform.tfstate"
+    region         = "eu-central-1"
+    dynamodb_table = "terraform-state-locks"
+    encrypt        = true
+  }
+}
+
+module "s3-bucket" {
+  source              = "github.com/larswillrich/terraforf-modules-example//s3-bucket"
+  organization        = "myinsurance"
+  organizationUnit    = "damagecase"
+  account             = "myinsurance-dev"
+  stage               = "prod"
+}
